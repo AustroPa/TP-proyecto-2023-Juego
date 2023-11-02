2596490cab796e2034ecb454c4e4c9d5f3f273a7
AustroPa <austroPa@gmail.com>
2023-11-02 16:08:27 -0300

    ataque del pj hasta la animacion, falta crearla

---
+++ Gato Travieso/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+	[SerializeField] private float attackCooldown;
+	private Animator anim;
+	private PlayerMovement playerMovement;
+	private float cooldownTimer = Mathf.Infinity;
+
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+		playerMovement = GetComponent<PlayerMovement>();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)	&&	cooldownTime > attackCooldown	&&	playerMovement.canAttack())
+			Attack();
+
+		cooldownTimer += Time.deltaTime;
+
+	public void Attack() {
+			anim.SetTrigger("attack");
+			cooldownTimer = 0;
+	}
+}
